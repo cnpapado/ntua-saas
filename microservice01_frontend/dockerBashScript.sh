ae1427df6d0493ddce68ac810ba5d964f716d99c
ellial <el18167@mail.ntua.gr>
2022-07-10 12:09:02 +0300

    - added docker compose script -added dockerignore
    To do:
    - Fix warnings
    - Add generic paths to working directory

--- microservice01_frontend/dockerBashScript.sh
+++ microservice01_frontend/dockerBashScript.sh
@@ -1,4 +1,2 @@
-# Build docker image
-docker build . -t sample-app
-# Run docker image
-docker run -it -p 3000:3000 sample-app
+# Start the container using specified docker compose file
+docker-compose -f docker-compose.dev.yaml up
