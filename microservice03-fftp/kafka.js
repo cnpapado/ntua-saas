1c53d836c67b8103dea87439784ba6d3677874a2
Constantinos Papadopoulos <14839869+cnpapado@users.noreply.github.com>
2022-07-10 22:03:25 +0300

    Switched to kafkajs

--- microservice03-fftp/kafka.js
+++ microservice03-fftp/kafka.js
@@ -1,78 +1,31 @@
-const Kafka = require('node-rdkafka');
+const { Kafka } = require('kafkajs');
+// import * as env from "env-var"
 
-const ERR_TOPIC_ALREADY_EXISTS = 36;
-
-function ensureTopicExists() {
-  const adminClient = Kafka.AdminClient.create({
-    'bootstrap.servers': "pkc-03vj5.europe-west8.gcp.confluent.cloud:9092",
-    'sasl.username': "OVB37BUHAGPWJKF5",
-    'sasl.password': "DVPX7TPSnSwiKSrGiXAONQPi7rjNVVHSe87PiOQQNsAi02IHzEtdw3cSyZOpGb0g",
-    'security.protocol': "SASL_SSL",
-    'sasl.mechanisms': "PLAIN"
-  });
-
-  return new Promise((resolve, reject) => {
-    adminClient.createTopic({
-      topic: "fft-csv",
-      num_partitions: 1,
-      replication_factor: 3
-    }, (err) => {
-      if (!err) {
-        console.log(`Created topic`);
-        return resolve();
-      }
-
-      if (err.code === ERR_TOPIC_ALREADY_EXISTS) {
-        return resolve();
-      }
-      return reject(err);
-    });
-  });
-}
-
-function createProducer(onDeliveryReport) {
-  const producer = new Kafka.Producer({
-    'bootstrap.servers': "pkc-03vj5.europe-west8.gcp.confluent.cloud:9092",
-    'sasl.username': "VUVCRRWVK4VCC4IE",
-    'sasl.password': "CIlD5L/yeLYdSf1or8PUHYHpQ03bNkKt5pKy8ibDXbhYOIvzAqMMsTcWzH8s2yma",
-    'security.protocol': "SASL_SSL",
-    'sasl.mechanisms': "PLAIN",
-    'dr_msg_cb': true
-  });
-
-  return new Promise((resolve, reject) => {
-    producer
-      .on('ready', () => resolve(producer))
-      .on('delivery-report', onDeliveryReport)
-      .on('event.error', (err) => {
-        console.warn('event.error', err);
-        reject(err);
-      });
-    producer.connect();
-  });
-}
-
-async function produce(key, value) {
-  await ensureTopicExists();
-  console.log("producing...")
-  const producer = await createProducer((err, report) => {
-    if (err) {
-      console.warn('Error producing', err)
-    } else {
-      const {topic, partition, value} = report;
-      console.log(`Successfully produced record to topic "${topic}" partition ${partition} ${value}`);
+const kafka = new Kafka({
+    clientId: "producer",
+    brokers: ["pkc-03vj5.europe-west8.gcp.confluent.cloud:9092"],  // [env.get("KAFKA_BOOTSTRAP_SERVER").required().asString()],
+    ssl: true,
+    sasl: { 
+        mechanism: 'plain', 
+        password: "CIlD5L/yeLYdSf1or8PUHYHpQ03bNkKt5pKy8ibDXbhYOIvzAqMMsTcWzH8s2yma", 
+        username: "VUVCRRWVK4VCC4IE" 
     }
-  });
+})
 
-  console.log(`Producing record ${key}\t${value}`);
+const producer = kafka.producer();
+module.exports = producer;
+// const run = async () => {
+//     await producer.connect()
+//     var i = 0
+//     while (true) {
+//         await producer.send({
+//             topic: "fft-csv",
+//             messages: [
+//                 { key: i.toString(), value: Math.random().toString() }
+//             ]
+//         })
+//         ++i
+//     }
+// }
 
-  producer.produce("fft-csv", -1, value, key);
-  
-  producer.flush(10000, () => {
-    producer.disconnect();
-  });
-}
-
-// module.exports = produce;
-
-produce("hey", "you");
+// run().catch(e => console.error(`[kafka-producer] ${e.message}`, e))
--- microservice03-fftp/publish_csv.js
+++ microservice03-fftp/publish_csv.js
@@ -1,26 +1,33 @@
+//node publish_csv.js -t fft-csv -f datasets/agpt/2022_01_01_03_AggregatedGenerationPerType16.1.BC.csv
+
 require("dotenv").config({path: "./config.env"});
-const path = require("path");
 const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers');
 const fs = require("fs");
 const csv = require("fast-csv");
 const {aws_download,get_availability,get_object_list} = require("./aws.js");
-const produce = require("./kafka.js");
-// const create_filename = require("./data_names.csv");
+const producer = require("./kafka.js");
+
+const produce_msg = async (i, row) => {
+    await producer.send({
+        topic: topic,
+        messages: [
+            { key: i.toString(), value: JSON.stringify(row)}
+        ]
+    })
+}
 
 function publish_csv(filename, topic) {
-    // let filename = "datasets/agpt/2022_01_01_03_AggregatedGenerationPerType16.1.BC.csv"; //create_filename
-    // console.log("filename ", filename)
-    aws_download(filename, () => {
+    // connect to the kafka producer
+    producer.connect();
+    aws_download(filename, async () => {
+        let i = 0;
         fs.createReadStream("./newfile.csv")
-            .pipe(csv.parse({ headers: true }))
+            .pipe(csv.parse({ headers: true, delimiter: '\t' }))
             .on('error', error => console.error("Error while reading temp csv file", error))
-            .on('data', row => {
-                produce()
-                    .catch((err) => {
-                        console.error(`Producer error:\n${err}`);
-                        process.exit(1);
-                    });
+            .on('data', (row) => {
+                produce_msg(i, row).catch(e => console.error(`[kafka-producer] ${e.message}`, e))
+                ++i
             })
             .on('end', rowCount => console.log(`Parsed ${rowCount} rows`));
         
@@ -28,12 +35,12 @@ function publish_csv(filename, topic) {
 };
 
 const argv = yargs(hideBin(process.argv)).argv
-// console.log(myArgs)
 let filename = argv.f;
 let topic = argv.t;
+
 if (topic == undefined || filename == undefined) {
     console.error(`Please specify both filename (-f) and topic (-t) command line options`)
     process.exit(1);
 }
-console.log(filename)
-publish_csv(filename, "fft-csv"); //(filename, topic)
+
+publish_csv(filename, topic)
