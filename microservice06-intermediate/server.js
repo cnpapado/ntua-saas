2dca8f87db11070410a8bc90833f7057bf197e08
Evangelia0 <euaggriz@yahoo.gr>
2022-07-17 20:37:07 +0300

    added header quantity

--- microservice06-intermediate/server.js
+++ microservice06-intermediate/server.js
@@ -13,20 +13,72 @@ app.use(cors({
 }));
 
 //route for Actual Total Load request
-app.route('/ATL').get(async (req,res)=>{
-        DateTime = new Date(req.header("DateFrom").toString())
-        MapCode  = countries[req.header("CountryFrom")];
+app.route('/').get(async (req,res)=>{
+        if(req.header("Quantity") === "1") {
+            DateTime = new Date(req.header("DateFrom").toString())
+            MapCode  = countries[req.header("CountryFrom")];
+            ret=[]
+            if(DateTime === undefined || MapCode === undefined){
+                ret.push({"Error":"Undefined value"})
+                console.log(DateTime,MapCode)
+                res.send(ret)
+            }
+            else {
+                var find = totalload.where("MapCode","==", MapCode).where("DateTime",">=",DateTime);
+                find.get().then(async function(querySnapshot) {
+                    if(querySnapshot.empty){
+                        ret.push({"Error":"No data available"})
+                    }
+                    else {
+                    querySnapshot.forEach(async function(doc) {
+                        ret.push(doc.data())
+                        })
+                    }
+                    res.send(ret);
+                    })
+            }    
+    }
+    if(req.header("Quantity").toString() === "2"){
+        DateTime          = new Date(req.header("DateFrom").toString())
+        MapCode           = countries[req.header("CountryFrom")];
+        ProductionType    = req.header("ProductionType");
         ret=[]
-        if(DateTime === undefined || MapCode === undefined){
+        if(DateTime === undefined || MapCode === undefined || ProductionType === undefined){
+            ret.push({"Error":"Undefined value"});
+            console.log(DateTime,MapCode,ProductionType);
+            res.send(ret);
+        }
+        else {
+            var find = agpt.where("MapCode","==", MapCode).where("ProductionType","==",ProductionType).where("DateTime",">=",DateTime);
+            find.get().then(async function(querySnapshot) {
+                if(querySnapshot.empty){
+                    ret.push({"Error":"No data available"});
+                }
+                else {
+                querySnapshot.forEach(async function(doc) {
+                    ret.push(doc.data())
+                    })
+                }
+                res.send(ret);
+                })  
+        } 
+    }
+    if(req.header("Quantity").toString() === "3"){
+        DateTime = new Date(req.header("DateFrom").toString())
+        InMapCode  = countries[req.header("CountryFrom")];
+        OutMapCode = countries[req.header("CountryTo")];
+        ret=[]
+        if(DateTime === undefined || InMapCode === undefined || OutMapCode === undefined){
             ret.push({"Error":"Undefined value"})
-            console.log(DateTime,MapCode)
+            console.log(DateTime,InMapCode,OutMapCode);
             res.send(ret)
         }
         else {
-            var find = totalload.where("MapCode","==", MapCode).where("DateTime",">=",DateTime);
+            var find = fft.where("InMapCode","==", InMapCode).where("OutMapCode","==", OutMapCode).where("DateTime",">=",DateTime);
+            
             find.get().then(async function(querySnapshot) {
                 if(querySnapshot.empty){
-                    ret.push({"Error":"No data available"})
+                    ret.push({"Error":"No data available"});
                 }
                 else {
                 querySnapshot.forEach(async function(doc) {
@@ -35,10 +87,12 @@ app.route('/ATL').get(async (req,res)=>{
                 }
                 res.send(ret);
                 })
-        }    
+        }
+    }
 });
 
 //route for Physical Flows request
+/*
 app.route('/FFT').get((req,res)=>{
     DateTime = new Date(req.header("DateFrom").toString())
     InMapCode  = countries[req.header("CountryFrom")];
@@ -91,6 +145,7 @@ app.route('/AGPT').get(async (req,res)=>{
             })  
     } 
 });
+*/
 
 app.listen(PORT, function() {
     console.log('Server is running on PORT:',PORT);
